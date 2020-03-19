da094d65ac96a968359c59490f412df08205af12
Mehdi Abbasi <57258796+mehdi-abbasi@users.noreply.github.com>
2020-03-20 01:09:33 +0330

    multi client

--- Client.py
+++ Client.py
@@ -1,14 +1,24 @@
 import socket
+import threading
 
+def msg_recieve():
+    while True:
+        msg = s.recvfrom(1024)
+        print (msg[0].decode())
+
+User_Name = input("Please Enter Your Username: ")
 s=socket.socket()
 
-port = 12345
+port = 8080
+s.connect(('127.0.0.1',port))
 
-s.connect(('127.0.0.1', port))
+s.send(User_Name.encode())
+
+
+msg_handler = threading.Thread(target=msg_recieve)
+msg_handler.start()
+
 while True:
-    msg = input('Enter your message:')
+    
+    msg = input("")
     s.send(msg.encode())
-    rmsg, bbb = s.recvfrom(1024)
-    print (rmsg.decode())
-    if rmsg.decode() == 'quit()':
-        break
