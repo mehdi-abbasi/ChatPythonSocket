60bd6b3a258fc34976dade66d78df9a9087f988d
Mehdi Abbasi <57258796+mehdi-abbasi@users.noreply.github.com>
2020-03-20 01:11:24 +0330

    multi client

--- Server.py
+++ Server.py
@@ -1,23 +1,43 @@
 import socket
+import threading
+import queue
 
-s = socket.socket()
-print('socket successfully created')
+def MSG_Broadcast(rmsg,rclient):
+    for client in Clients:
+        try:
+            if Clients[client] != rclient:
+                Clients[client].send(rmsg)
+        except:
+            print("ERROR BROADCASTING")
 
-port = 12345
+def conn_accept(myq):
+    while True:
+        client = server.accept()
+        client[0].settimeout(0.0001)
+        msg = client[0].recvfrom(1024)
+        msg = msg[0].decode()
+        print ('got connection from', msg)
+        client_dict = {msg : client[0]}
+        myq.put(client_dict)
 
-s.bind(('', port))
-print ('socket binded to ',port)
+q = queue.Queue()
+Clients = {}
 
-s.listen(5)
-print ('socket is listening')
-client, address = s.accept()
-print ('got connection from', address)
+addr = ("127.0.0.1",8080)
+server = socket.create_server(addr,family=socket.AF_INET,)
+
+accept_thread = threading.Thread(target=conn_accept,args=(q,))
+accept_thread.start()
+
 while True:
-        
-        
-        MSG, bbb = client.recvfrom(1024)
-        print (MSG.decode())
-        m = input('Enter your message:')
-        client.send(m.encode())
-        if MSG.decode() == 'quit()':
-           break
+    if not q.empty():
+        c = q.get()
+        Clients.update(c)
+    for client in Clients:
+        try:
+            msg = Clients[client].recvfrom(1024)
+            msg = client + ": " + msg[0].decode()
+            msg = msg.encode()
+            MSG_Broadcast(msg,Clients[client])
+        except:
+            pass
